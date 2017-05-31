1603017c563befacacde91da3cd13020a834644f
Qun <slvt@qq.com>
2017-05-31 16:56:13 +0800

    设置闪屏界面去状态栏和标题栏

--- app/src/main/java/com/qun/mobilesafe/act/SplashActivity.java
+++ app/src/main/java/com/qun/mobilesafe/act/SplashActivity.java
@@ -5,11 +5,15 @@ import android.os.Bundle;
 
 import com.qun.mobilesafe.R;
 
+/**
+ * Created by Qun on 2017/5/31.
+ */
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
     }
 }
