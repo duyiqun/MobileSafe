f281c4f1533fedebc52c8294c08b8ba09f952b1b
Qun <slvt@qq.com>
2017-05-31 17:47:36 +0800

    获取版本名并生成工具类

--- app/src/main/java/com/qun/mobilesafe/act/SplashActivity.java
+++ app/src/main/java/com/qun/mobilesafe/act/SplashActivity.java
@@ -1,19 +1,52 @@
 package com.qun.mobilesafe.act;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.qun.mobilesafe.R;
-
-/**
- * Created by Qun on 2017/5/31.
- */
+import com.qun.mobilesafe.utils.PackageUtil;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private TextView mSplashTvVersion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        initView();
+        initData();
+    }
+
+    private void initView() {
+        mSplashTvVersion = (TextView) findViewById(R.id.splash_tv_version);
+    }
+
+    private void initData() {
+        //获取版本名展示
+        initVersionName();
+
+        //自动更新功能
+        upDateVersion();
+    }
+
+    private void initVersionName() {
+//        PackageManager manager = getPackageManager();
+//        //参数一：要查询的应用包信息对应包名，参数二：标记，想获取什么信息，就设置什么标记（0：基本信息）
+//        try {
+//            PackageInfo packageInfo = manager.getPackageInfo(getPackageName(), 0);
+//            String versionName = packageInfo.versionName;
+//            mSplashTvVersion.setText(versionName);
+//        } catch (PackageManager.NameNotFoundException e) {
+//            e.printStackTrace();
+//        }
+        String versionName = PackageUtil.getVersionName(SplashActivity.this, getPackageName());
+        mSplashTvVersion.setText(versionName);
+    }
+
+    private void upDateVersion() {
+
     }
 }
---
+++ app/src/main/java/com/qun/mobilesafe/utils/PackageUtil.java
@@ -0,0 +1,42 @@
+package com.qun.mobilesafe.utils;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
+/**
+ * Created by Qun on 2017/5/31.
+ */
+
+public class PackageUtil {
+
+    //获取版本名
+    public static String getVersionName(Context context, String packageName) {
+        PackageManager manager = context.getPackageManager();
+        //参数一：要查询的应用包信息对应包名，参数二：标记，想获取什么信息，就设置什么标记（0：基本信息）
+        String versionName = "未知";
+        try {
+            PackageInfo packageInfo = manager.getPackageInfo(packageName, 0);
+            versionName = packageInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return versionName;
+    }
+
+    //获取版本号
+    public static int getVersionCode(Context context, String packageName) {
+        PackageManager manager = context.getPackageManager();
+        //参数一：要查询的应用包信息对应包名，参数二：标记，想获取什么信息，就设置什么标记（0：基本信息）
+        int versionCode = 1;
+        try {
+            PackageInfo packageInfo = manager.getPackageInfo(packageName, 0);
+            versionCode = packageInfo.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return versionCode;
+    }
+}
