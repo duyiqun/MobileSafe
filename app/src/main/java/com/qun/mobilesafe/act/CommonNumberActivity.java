cd1e044a5458d3a2ecf2541f84830da4b9d95377
Qun <slvt@qq.com>
2017-06-06 16:28:24 +0800

    设置ExpandableListView的点击事件

--- app/src/main/java/com/qun/mobilesafe/act/CommonNumberActivity.java
+++ app/src/main/java/com/qun/mobilesafe/act/CommonNumberActivity.java
@@ -1,6 +1,8 @@
 package com.qun.mobilesafe.act;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -15,6 +17,7 @@ import com.qun.mobilesafe.db.CommonNumberDb;
 public class CommonNumberActivity extends AppCompatActivity {
 
     private ExpandableListView mElv;
+    private int prevousExpandedPosition = -1;//记录前一个被展开的组的索引
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +31,47 @@ public class CommonNumberActivity extends AppCompatActivity {
         mElv = (ExpandableListView) findViewById(R.id.elv);
         MyAdapter adapter = new MyAdapter();
         mElv.setAdapter(adapter);
+
+        //给组设置点击事件
+        mElv.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
+
+                //当点击时，当前展开的索引不是-1并且，我们点击的组的索引与记录下来的前一个展开的索引不同时，将前一个展开的组给关闭
+                if (prevousExpandedPosition != -1 && prevousExpandedPosition != groupPosition) {
+                    mElv.collapseGroup(prevousExpandedPosition);
+                }
+
+                //根据当前组的状态，将组的打开与关闭进行逻辑操作
+                if (mElv.isGroupExpanded(groupPosition)) {//当前是打开状态
+                    mElv.collapseGroup(groupPosition);//关闭对应的组
+                } else {//当前是关闭状态
+                    mElv.expandGroup(groupPosition);//展开对应的组
+                    prevousExpandedPosition = groupPosition;
+                }
+                mElv.setSelection(groupPosition);//能够将点击的组的条目直接显示到第一个可见条目上(可以实现回到顶部的效果)
+
+//                return false;//组的点击事件不由我们自己处理，由ExpandableListView源码处理
+                return true;//组的点击事件由我们自己处理
+            }
+        });
+
+        //子条目的点击事件
+        mElv.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
+                //获取对应条目的号码，并拨打出去
+                String[] childText = CommonNumberDb.getChildText(CommonNumberActivity.this, groupPosition, childPosition);
+                String number = childText[1];
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_DIAL);//进入拨号界面，但是不进行拨打
+                intent.setData(Uri.parse("tel:" + number));
+                startActivity(intent);
+                return true;
+            }
+        });
     }
 
     private class MyAdapter extends BaseExpandableListAdapter {
